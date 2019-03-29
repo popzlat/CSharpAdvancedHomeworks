ea8412cec20303245b34af3c8b76313721f977f9
popzlat <zlatko.p4@gmail.com>
2019-03-29 23:51:09 +0100

    Homework commit

---
+++ Homework01/Homework01/InterfacesHomework/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPredator shark = new Fish() { AttackSpeed = 30, Name = "Shark" };
+            IPray pinguin = new Fish() { FleeSpeed = 25, Name = "Pinguin" };
+            IPray herring = new Fish() { FleeSpeed = 35, Name = "Herring" };
+            shark.Attack(pinguin);
+            shark.Attack(herring);
+            Console.WriteLine();
+            //---------------------------------
+
+            IPredator gepard = new Cat() { AttackSpeed = 60, Name = "Gepard" } ;
+            IPray gazella = new Gazella() { FleeSpeed = 55, Name = "Gazella" };
+            IPredator tiger = new Cat() {AttackSpeed = 70,Name = "Tiger"};
+            tiger.Attack(pinguin);
+            pinguin.Flee();
+            gepard.Attack(gazella);
+            IPredator lion = new Cat { AttackSpeed = 35, Name = "Lion" };
+            lion.Attack(gazella);
+            
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
