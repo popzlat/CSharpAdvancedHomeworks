ecaa75e0c01415bc263625ad72b1c06ba5d7e4ad
Zlatko Poposki <45499958+popzlat@users.noreply.github.com>
2019-04-02 14:35:38 +0200

    Add files via upload

---
+++ Exercise01/Exercise01/Exercise01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise01.StringExtension;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pharse;
+            pharse = "Madam, I'm Adam";
+            Console.WriteLine(pharse.IsPalindrom()); //true
+            pharse = "Madam, I am Adam";
+            Console.WriteLine(pharse.IsPalindrom()); // false
+            pharse = "Refer, refer!";
+            Console.WriteLine(pharse.IsPalindrom()); // true
+            double numeric;
+            numeric = 123.321;
+            Console.WriteLine(numeric.IsPalindrom()); // true
+            int integer;
+            integer = 11211;
+            Console.WriteLine(integer.IsPalindrom()); // true
+            long bigint;
+            bigint = 123456654321;
+            Console.WriteLine(bigint.IsPalindrom()); // true
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise01/Exercise01/Exercise01/StringExtension/Extensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01.StringExtension
+{
+    public static class Extensions
+    {
+        public static string ReverseString(this string text)
+        {
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--) {
+                reverse += cArray[i];
+            }
+            return reverse;
+
+        }
+        public static bool IsPalindrom(this string text )
+        {
+            string newString = text.Replace(" ", "").Replace(",", "").Replace("'", "").Replace("!","").ToLower();
+            string reversed = newString.ReverseString().ToLower();
+            if (newString == reversed) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        public static bool IsPalindrom(this int number)
+        {
+            string strNum = number.ToString();
+            char[] array = strNum.ToCharArray();
+            return CharArray(array);
+
+
+        }
+        public static bool IsPalindrom(this long number)
+        {
+            string strNum = number.ToString();
+
+            char[] array = strNum.ToCharArray();
+            return CharArray(array);
+
+
+
+        }
+        public static bool IsPalindrom(this double number)
+        {
+            string strNum = number.ToString();
+            char[] array = strNum.ToCharArray();
+            return CharArray(array);
+
+        }
+        public static bool CharArray (this char[] array)
+        {
+            string orgArray = new string(array);
+            Array.Reverse(array);
+            string reversed = new string(array);
+            return orgArray == reversed;
+        }
+       
+
+    }
+
+
+}
+
