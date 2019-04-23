0384396324b7fff7777ea1d54aa4faf844bef3db
Zlatko Poposki <45499958+popzlat@users.noreply.github.com>
2019-04-23 15:51:53 +0200

    Add files via upload

---
+++ Homework05/Homework05/ConsoleApp1/Customer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Customer : IEquatable<Customer>
+    {
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int MonthlySpend { get; set; }
+
+        public Customer() { }
+        public Customer(string name, string address, string city, int monthlySpend)
+        {
+            this.Name = name; this.Address = address; this.City = city; this.MonthlySpend = monthlySpend;
+        }
+        public void PrintCustomer()
+        {
+            Console.WriteLine($"Customer: {Name}, {Address}, {City}, {MonthlySpend}");
+        }
+        public int CompareTo(Customer c)
+        {
+            return this.MonthlySpend.CompareTo(c.MonthlySpend);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public bool Equals(Customer c)
+        {
+            if (c == null || this.GetType().Equals(c.GetType())) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public static bool operator ==(Customer a, Customer b)
+        {
+            if (object.ReferenceEquals(a, null)) {
+                if (object.ReferenceEquals(b, null)) {
+                    return true;
+                }
+                return false;
+            }
+            if (object.ReferenceEquals(b, null)) {
+                if (object.ReferenceEquals(a, null)) {
+                    return true;
+                }
+                return false;
+            }
+            return a.Name == b.Name && a.Address == b.Address && a.City == b.City;
+        }
+        public static bool operator !=(Customer a, Customer b)
+        {
+            return a.Name == b.Name && a.Address == b.Address && a.City == b.City;
+        }
+
+
+
+
+
+
+
+    }
+}
+
+
---
+++ Homework05/Homework05/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer("igor", "partizanska 95", "Skopje", 30000);
+            Customer c2 = new Customer("dejan", "roza luxemburg 1", "Skopje", 25000);
+            Customer c3 = new Customer("ivan", "partizanska 95", "Strumica", 29000);
+            Customer c5 = new Customer("petar", "vodjanska 95", "Bitola", 35000);
+            Customer c6 = new Customer("igor", "partizanska 95", "Skopje", 30000);
+
+            Customer[] customers = new Customer[] { c1, c2, c3, c5, c5 };
+
+            
+            Console.WriteLine("Equality c1 and c5 {0}", c1.Equals(c5));
+
+            if (c1 == c5) {
+                Console.WriteLine("c1 == c5");
+            }
+            else
+                Console.WriteLine("c1 != c5");
+
+
+            Console.ReadLine();
+        }
+    }
+}
